e38d836636fcc1654651cdd6776acd48eb3451c0
@IssamIstars <issam.barhoumi@istars.net>
2023-12-15 21:41:57 +0100

    Projet_Django

---
+++ glsip/glsip/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for glsip project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'glsip.settings')
+
+application = get_asgi_application()
